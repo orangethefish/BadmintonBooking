ed2fb719b15c0d6cae58ef6ae12605f36fdf6216
orangethefish <duyhoa2210@gmail.com>
2025-05-18 00:07:59 +0700

    Add i18nsupport and placeid for facility

--- Frontend/src/app/services/facility.service.ts
+++ Frontend/src/app/services/facility.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Facility, ResolveUrlResponse } from '../models/facility.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,26 +12,27 @@ export class FacilityService {
 
   constructor(private http: HttpClient) {}
 
-  createFacility(facility: any): Observable<any> {
-    return this.http.post(this.apiUrl, facility);
+  createFacility(facility: any): Observable<Facility> {
+    return this.http.post<Facility>(this.apiUrl, facility);
   }
 
-  getFacility(id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${id}`);
+  getFacility(id: number): Observable<Facility> {
+    return this.http.get<Facility>(`${this.apiUrl}/${id}`);
   }
 
-  updateFacility(id: number, facility: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, facility);
+  updateFacility(id: number, facility: any): Observable<Facility> {
+    return this.http.put<Facility>(`${this.apiUrl}/${id}`, facility);
   }
 
   deleteFacility(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
-  getFacilities(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getFacilities(): Observable<Facility[]> {
+    return this.http.get<Facility[]>(this.apiUrl);
   }
-  resolveUrl(url: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/resolve-url`, { url });
+  
+  resolveUrl(url: string): Observable<ResolveUrlResponse> {
+    return this.http.post<ResolveUrlResponse>(`${this.apiUrl}/resolve-url`, { url });
   }
 } 
