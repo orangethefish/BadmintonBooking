0bd7153a754425c2f3fd497b9176dcad7a11877f
orangethefish <duyhoa2210@gmail.com>
2025-05-19 19:07:22 +0700

    [Chore] BB-29: add Backend.Tests to solution file

--- Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08c0bed72d9be57b9781cc9aedd0be3e283b418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174f4bb474ab33d15a367d1efbe21c9ce7113f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
